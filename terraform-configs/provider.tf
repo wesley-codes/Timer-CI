822b95aaf6abc9a5dc7dfef583e6d1e325656ebc
Meka <ezemikey12@gmail.com>
2025-12-23 21:35:39 -0500

    Add more configs

--- terraform-configs/provider.tf
+++ terraform-configs/provider.tf
@@ -5,3 +5,10 @@ provider "kubernetes" {
     cluster_ca_certificate = minikube_cluster.minikube_docker.cluster_ca_certificate
 }
 
+
+provider "helm" {
+    kubernetes = {
+      config_path = "~/.kube/config"
+    }
+  
+}
